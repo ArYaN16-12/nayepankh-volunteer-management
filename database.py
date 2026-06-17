6bd24d9566165118ff576d847817b1bfcf0ac69d
ArYaN16-12 <chaudharyaryan56004@gmail.com>
2026-06-17 17:18:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import mysql.connector
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="kakarot",
-        database="nayepankh"
+        host = "localhost"
+        user = "your_mysql_username"
+        password = "your_mysql_password"
+        database = "nayepankh"
     )
     return connection
